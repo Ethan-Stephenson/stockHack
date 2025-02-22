a3ad0f061891556d4719c0e3fc575b490f83a8c6
Voulu-2k6 <tms6415@gmail.com>
2025-02-22 12:08:24 -0600

    :q

--- python/test.py
+++ python/test.py
@@ -1,6 +1,27 @@
-import finnhub
-import constants
+#import finnhub
+#import constants
 
+#finnhub_client = finnhub.Client(api_key=f"{constants.FINNHUB_API_KEY}")
 
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
 
-finnhub_client = finnhub.Client(api_key=f"{constants.FINNHUB_API_KEY}")
+data = yf.download("CELH", start="2025-01-01", end="2025-02-21")
+
+#with open("TestFile.csv", "w") as file:
+#    data.to_csv("TestFile.csv", index=False)
+
+print(data.head())
+print(data.info())
+print(data.describe())
+print(data.shape)
+
+# Visualizing the opening prices of the data.
+plt.figure(figsize=(16,8))
+plt.title('Celsuis')
+plt.xlabel('Days')
+plt.ylabel('Opening Price USD ($)')
+plt.plot(data['open'])
+plt.show()
+
