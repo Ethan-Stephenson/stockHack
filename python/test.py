382911cade884a531ad00ee2e8184c2e98a7d460
Voulu-2k6 <tms6415@gmail.com>
2025-02-22 12:15:39 -0600

    CELH.csv data

--- python/test.py
+++ python/test.py
@@ -7,21 +7,24 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = yf.download("CELH", start="2025-01-01", end="2025-02-21")
+data = yf.download("CELH", start="2024-06-01", end="2025-02-22")
 
-#with open("TestFile.csv", "w") as file:
-#    data.to_csv("TestFile.csv", index=False)
+with open("CELH.csv", "w") as file:
+    data.to_csv("CELH.csv", index=False)
+
+with open("CELH.csv", "a") as file:
+    file.write("From 2024-06-03 to 2025-01-22")
 
 print(data.head())
-print(data.info())
-print(data.describe())
-print(data.shape)
+#print(data.info())
+#print(data.describe())
+#print(data.shape)
 
 # Visualizing the opening prices of the data.
-plt.figure(figsize=(16,8))
-plt.title('Celsuis')
-plt.xlabel('Days')
-plt.ylabel('Opening Price USD ($)')
-plt.plot(data['open'])
-plt.show()
+#plt.figure(figsize=(16,8))
+#plt.title('Celsuis')
+#plt.xlabel('Days')
+#plt.ylabel('Opening Price USD ($)')
+#plt.plot(data['open'])
+#plt.show()
 
