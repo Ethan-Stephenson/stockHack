24d30196949234739cf629a8ebaebf49ec054f34
Ethan Stephenson <steph971@umn.edu>
2025-02-22 12:12:45 -0600

    push

--- python/test.py
+++ python/test.py
@@ -1,6 +1,14 @@
 import finnhub
 import constants
-
+import pandas as pd
 
 
 finnhub_client = finnhub.Client(api_key=f"{constants.FINNHUB_API_KEY}")
+
+res = finnhub_client.company_news('CELH', _from="2022-06-01", to="2024-06-10")
+
+
+
+
+print(pd.DataFrame(res))
+
