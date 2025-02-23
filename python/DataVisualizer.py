c4970a950a14d63251586b622c4018ab86f6a576
Voulu-2k6 <tms6415@gmail.com>
2025-02-23 12:03:59 -0600

    Data Visualizer is the only important file. DecisionTreeRegressor is nothing for now

--- python/DataVisualizer.py
+++ python/DataVisualizer.py
@@ -4,47 +4,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #### Edit range of data
-data = yf.download("FUBO", start="2025-01-01", end="2025-02-22")
+data = yf.download("ALT", start="2024-01-01", end="2025-02-22")
 
-data2 = pd.DataFrame(columns=['close'])
+data2 = pd.DataFrame(columns=['close', 'yestClose', 'volume'])
 data2['close'] = data["Close"]
+data2['volume'] = data["Volume"]
 data2 = pd.DataFrame(data2)
 
 #### Number of days to predict
 shift = 3
 
-data2['Prediction'] = data2.shift(-shift)
+data2['yestClose'] = data2['close'].shift(1)
+data2['Prediction'] = data2['close'].shift(-shift)
 
-#CONTENT WARNING: IDK WHAT ANY OF THIS DOES vvvv
+print(data2)
+
+# X: our training variables
 X = np.array(data2.drop(columns='Prediction'))[:-shift]
-#print(X)
 
+# Y: our stored predictions
 y = np.array(data2['Prediction'])[:-shift]
-#print(y)
 
+# We have yet to use x_test or y_test. Ignore for now. 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
-
-# Implementing Linear and Decision Tree Regression Algorithms.
+# Implementing Decision Tree Regression Algorithms.
 tree = DecisionTreeRegressor().fit(x_train, y_train)
-lr = LinearRegression().fit(x_train, y_train)
 
 x_future = data2.drop(columns='Prediction')[:-shift]
 x_future = x_future.tail(shift)
 x_future = np.array(x_future)
+print(x_future)
 
 tree_prediction = tree.predict(x_future)
 print(tree_prediction)
 
-lr_prediction = lr.predict(x_future)
-print(lr_prediction)
-
-predictions = tree_prediction 
+prediction2 = tree_prediction
 valid = data2[X.shape[0]:]
-valid['Predictions'] = predictions
+valid['Prediction2'] = prediction2
 
 #Something happens
 plt.figure(figsize=(16,8))
@@ -52,6 +52,7 @@ plt.title("Model")
 plt.xlabel('Days')
 plt.ylabel('Close Price USD ($)')
 plt.plot(data2['close'])
-plt.plot(valid[['close', 'Predictions']])
-plt.legend(["Original", "Valid", 'Predicted'])
+plt.plot(valid[['close', 'Prediction2']])
+plt.legend(["Original", "Valid", "Tree Prediction"])
 plt.show()
+
