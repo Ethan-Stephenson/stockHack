d284f5185d32ac1b51d97fd33f8e9eff0a5b3693
Voulu-2k6 <tms6415@gmail.com>
2025-02-23 18:12:06 -0600

    all 3 variables

--- python/DataVisualizer.py
+++ python/DataVisualizer.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #### Edit range of data
-data = yf.download("ALT", start="2025-01-01", end="2025-02-22")
+data = yf.download("CELH", start="2025-02-01", end="2025-02-24")
 
-data2 = pd.DataFrame(columns=['close3'])
-data2['close3'] = data["Close"]
+data2 = pd.DataFrame(columns=['close', 'yestClose', 'volume'])
+data2['close'] = data["Close"]
+data2['volume'] = data['Volume']
+data2['yestClose'] = data2['close'].shift(1)
 data2 = pd.DataFrame(data2)
 
 #### Number of days to predict
 shift = 3
 
-data2['Prediction'] = data2['close3'].shift(-shift)
+data2['Prediction'] = data2['close'].shift(-shift)
 #print(data2)
 
 # X: our training variables
@@ -55,7 +57,7 @@ future = pd.DataFrame({'Date': future_dates, 'Prediction': valid['Prediction'].v
 # Set 'Date' as the index
 future.set_index('Date', inplace=True)
 
-historical_data = data2['close3']
+historical_data = data2['close']
 
 print(historical_data)
 print(future)
@@ -65,7 +67,6 @@ plt.figure(figsize=(16,8))
 plt.title("Model")
 plt.xlabel('Days')
 plt.ylabel('Close Price USD ($)')
-
 
 # Extract the last historical data point
 last_date = historical_data.index[-1]
@@ -77,6 +78,7 @@ first_future_price = future.iloc[0]['Prediction']
 
 # Plot a connecting line
 plt.plot([last_date, first_future_date], [last_price, first_future_price], color='green', linestyle='dashed', label="Connecting Line")
+
 
 # Update legend to include the new line
 plt.legend(["Original", "Tree Prediction", "Connecting Line"])
